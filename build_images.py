7f9d0f6d2eb9672967aa4be8d90c8d18fb451111
Remo Andreoli <andreoliremo@gmail.com>
2021-10-26 18:56:27 +0200

    Moved Dockerfiles to a folder

--- build_images.py
+++ build_images.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import os
 
-os.system("sudo docker build . -f Dockerfile.env -t rt-mongod-env")
-os.system("sudo docker build . -f Dockerfile.build -t rt-mongod-build")
-os.system("sudo docker build . -f Dockerfile.run -t rt-mongod")
+os.system("sudo docker build . -f ./MongoDB/Dockerfile.env -t rt-mongod-env")
+os.system("sudo docker build . -f ./MongoDB/Dockerfile.build -t rt-mongod-build")
+os.system("sudo docker build . -f ./MongoDB/Dockerfile.run -t rt-mongod")
